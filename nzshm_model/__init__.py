bae4b9629a8be21f55df4524f1cb460dbf934a3e
chrisdicaprio <christopher.dicaprio@gmail.com>
2023-03-23 15:22:26 +1300

    register model NSHM_1.0.4

--- nzshm_model/__init__.py
+++ nzshm_model/__init__.py
@@ -1,11 +1,15 @@
 from . import nshm_1_0_0
+from . import nshm_1_0_4
 
 # Python package version is different than the NSHM MODEL version !!
 __version__ = '0.1.1'
 
 CURRENT_VERSION = "NSHM_1.0.0"
 
-versions = {"NSHM_1.0.0": nshm_1_0_0}
+versions = {
+    "NSHM_1.0.0": nshm_1_0_0,
+    "NSHM_1.0.4": nshm_1_0_4,
+    }
 
 
 def get_model_version(version_id):
