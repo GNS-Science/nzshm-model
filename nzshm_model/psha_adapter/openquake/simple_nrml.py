4c52a651e313417743cf7dfe48ecc9fadb04f862
Chris Chamberlain <chrisbc@artisan.co.nz>
2023-12-05 14:55:33 +1300

    let simple_nrml module initialise without optional dependencies;

--- nzshm_model/psha_adapter/openquake/simple_nrml.py
+++ nzshm_model/psha_adapter/openquake/simple_nrml.py
@@ -3,11 +3,15 @@ import pathlib
 import zipfile
 from typing import Union
 
-from nshm_toshi_client.toshi_file import ToshiFile
-
 from nzshm_model.psha_adapter.openquake.logic_tree import NrmlDocument
 from nzshm_model.psha_adapter.psha_adapter_interface import PshaAdapterInterface
 from nzshm_model.source_logic_tree.toshi_api import get_secret
+
+try:
+    from nshm_toshi_client.toshi_file import ToshiFile
+except (ModuleNotFoundError, ImportError):
+    print("WARNING: optional toshi dependency `nshm_toshi_client` is not installed")
+
 
 # Get API key from AWS secrets manager
 API_URL = os.getenv('NZSHM22_TOSHI_API_URL', "http://127.0.0.1:5000/graphql")
