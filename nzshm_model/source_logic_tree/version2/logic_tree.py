b943b25711bc9d2bc74b20d84b330e95fa0bd8e0
Chris Chamberlain <chrisbc@artisan.co.nz>
2023-12-10 14:29:39 +1300

    slt migration complete, without rate_scaling

--- nzshm_model/source_logic_tree/version2/logic_tree.py
+++ nzshm_model/source_logic_tree/version2/logic_tree.py
@@ -3,6 +3,7 @@
 """
 Define source logic tree structures used in NSHM.
 """
+import copy
 import json
 import pathlib
 from dataclasses import dataclass, field
@@ -20,7 +21,7 @@ from ..version1 import SourceLogicTree as SourceLogicTreeV1
 @dataclass
 class InversionSource:
     nrml_id: str
-    rupture_rate_scaling: Union[float, None]
+    rupture_rate_scaling: Union[float, None] = None  # TODO: needed at this level??
     inversion_id: Union[str, None] = ""
     rupture_set_id: Union[str, None] = ""
     inversion_solution_type: Union[str, None] = ""
@@ -30,7 +31,7 @@ class InversionSource:
 @dataclass
 class DistributedSource:
     nrml_id: str
-    rupture_rate_scaling: Union[float, None]
+    rupture_rate_scaling: Union[float, None] = None  # TODO: needed at this level??
     type: str = "distributed"
 
     # onfault_nrml_id: Union[str, None] = ""
@@ -99,6 +100,23 @@ class SourceLogicTree:
         if not isinstance(original_slt, SourceLogicTreeV1):
             raise ValueError(f"supplied object of {type(original_slt)} is not supported.")
         slt = SourceLogicTree(version=original_slt.version, title=original_slt.title)
+        for fslt in original_slt.fault_systems:
+            new_fslt = FaultSystemLogicTree(short_name=fslt.short_name, long_name=fslt.long_name)
+            for branch in fslt.branches:
+                # TODO: handle rate scaling
+                new_branch = Branch(values=copy.deepcopy(branch.values), weight=branch.weight)
+                if branch.onfault_nrml_id:
+                    new_branch.sources.append(
+                        InversionSource(
+                            nrml_id=branch.onfault_nrml_id,
+                            inversion_id=branch.inversion_solution_id,
+                            rupture_set_id=branch.rupture_set_id,
+                        )
+                    )
+                if branch.distributed_nrml_id:
+                    new_branch.sources.append(DistributedSource(nrml_id=branch.distributed_nrml_id))
+                new_fslt.branches.append(new_branch)
+            slt.fault_systems.append(new_fslt)
         return slt
 
     def psha_adapter(self, provider: Type[PshaAdapterInterface], **kwargs):
--- tests/test_slt_version_migration.py
+++ tests/test_slt_version_migration.py
@@ -1,21 +1,30 @@
 """
-test ability to migrate a v1 slt to v2
+test migration from slt v1 to v2
 """
-
-# from pathlib import Path
+import pytest
 
 import nzshm_model
 from nzshm_model.source_logic_tree.version2 import SourceLogicTree
 
 
-def test_migrate_NSHM_v1_0_4():
-
+@pytest.fixture(scope='module')
+def slt_version_1():
     # get a published v1 logic tree
+    yield nzshm_model.get_model_version('NSHM_v1.0.4').source_logic_tree()
 
-    slt = nzshm_model.get_model_version('NSHM_v1.0.4').source_logic_tree()
 
-    v2 = SourceLogicTree.from_source_logic_tree(slt)
+def test_migrate_NSHM_v1_0_4(slt_version_1):
+    v2 = SourceLogicTree.from_source_logic_tree(slt_version_1)
+    assert v2.version == slt_version_1.version
 
-    assert v2.version == slt.version
-    # assert v2.fault_systems[0].short_name == slt.fault_systems[0].short_name
-    # assert 0
+
+def test_migrate_NSHM_v1_0_4_fault_systems(slt_version_1):
+    v2 = SourceLogicTree.from_source_logic_tree(slt_version_1)
+    assert v2.fault_systems[0].short_name == slt_version_1.fault_systems[0].short_name
+
+
+def test_migrate_NSHM_v1_0_4_branches(slt_version_1):
+    v2 = SourceLogicTree.from_source_logic_tree(slt_version_1)
+    assert len(v2.fault_systems[0].branches) == len(slt_version_1.fault_systems[0].branches)
+    assert len(v2.fault_systems[0].branches) == len(slt_version_1.fault_system_lts[0].branches)  # old attributes
+    assert v2.fault_systems[0].branches[0].weight == slt_version_1.fault_systems[0].branches[0].weight
