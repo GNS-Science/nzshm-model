5a0cba8e81c98851b05069772854728dded46017
Chris B Chamberlain <chrisbc@artisan.co.nz>
2023-12-21 11:13:18 +1300

    Fix/missing fault system lts (#35)
    restore the old api property with a deprecation warning.

--- tests/test_module_import.py
+++ tests/test_module_import.py
@@ -4,25 +4,35 @@ import pytest
 import nzshm_model
 
 
-class TestStructure:
-    def test_current_version(self):
-        assert nzshm_model.CURRENT_VERSION == "NSHM_v1.0.0"
+def test_current_version():
+    assert nzshm_model.CURRENT_VERSION == "NSHM_v1.0.0"
 
-    def test_available_versions(self):
-        assert len(nzshm_model.versions) == 2
 
-    @pytest.mark.parametrize("model, model_version", [('NSHM_v1.0.0', 'NSHM_v1.0.0'), ('NSHM_v1.0.4', 'NSHM_v1.0.4')])
-    def test_version_config(self, model, model_version):
-        mod = nzshm_model.get_model_version(model)
+def test_available_versions():
+    assert len(nzshm_model.versions) == 2
 
-        assert mod.version == model_version
 
-        assert mod.slt_config is not None
-        assert mod.slt_config.logic_tree_permutations is not None
+@pytest.mark.parametrize("model, model_version", [('NSHM_v1.0.0', 'NSHM_v1.0.0'), ('NSHM_v1.0.4', 'NSHM_v1.0.4')])
+def test_version_config(model, model_version):
+    mod = nzshm_model.get_model_version(model)
 
-        # slt = mod.source_logic_tree()
-        # assert slt.version == model_version
+    assert mod.version == model_version
 
-    def test_get_model_version_unknown(self):
-        unknown = nzshm_model.get_model_version('XXX')
-        assert unknown is None
+    assert mod.slt_config is not None
+    assert mod.slt_config.logic_tree_permutations is not None
+
+
+def test_get_model_version_unknown():
+    unknown = nzshm_model.get_model_version('XXX')
+    assert unknown is None
+
+
+@pytest.mark.parametrize('model_version', [ver for ver in nzshm_model.versions.keys()])
+def test_source_logic_tree_in_all_models(model_version):
+    current_model = nzshm_model.get_model_version(model_version)
+    slt = current_model.source_logic_tree()
+
+    old_api_fslt = slt.fault_system_lts[0]
+    new_api_fslt = slt.fault_systems[0]
+
+    assert old_api_fslt is new_api_fslt
