1581180953ceccaac48dc330d068275767f8df31
chrisdicaprio <christopher.dicaprio@gmail.com>
2024-01-11 10:18:11 +1300

    Bump version: 0.6.0 → 0.7.0

--- nzshm_model/__init__.py
+++ nzshm_model/__init__.py
@@ -1,7 +1,7 @@
 from . import nshm_v1_0_0, nshm_v1_0_4
 
 # Python package version is different than the NSHM MODEL version !!
-__version__ = '0.6.0'
+__version__ = '0.7.0'
 
 CURRENT_VERSION = "NSHM_v1.0.0"
 
