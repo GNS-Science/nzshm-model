ca0b4fdb58bcffd543682813cf4d58c5a9b59186
ZeyuanC <z.chen@gns.cri.nz>
2024-02-07 12:13:16 +1300

    Made the branch_set argument more versatile (with related tests)

--- tests/test_branch_set.py
+++ tests/test_branch_set.py
@@ -2,16 +2,43 @@ import pytest
 
 from nzshm_model.branch_prop import get_branch_set
 
+# def test_get_branch_set():
+#     assert len(list(get_branch_set('NSHM_v1.0.4', 'CRU').branches)) == 36
 
-def test_get_branch_set():
-    assert len(list(get_branch_set('NSHM_v1.0.4', 'CRU').branches)) == 36
+
+# def test_get_branch_set_invalid_model_version():
+#     with pytest.raises(ValueError):
+#         get_branch_set('', 'CRU')
+
+
+# def test_get_branch_set_invalid_branch():
+#     with pytest.raises(ValueError):
+#         get_branch_set('NSHM_v1.0.4', 'AAA')
+
+
+def test_get_branch_set_with_list():
+    assert len(get_branch_set('NSHM_v1.0.4', ['CRU', 'PUY'])) == 2
+    assert get_branch_set('NSHM_v1.0.4', ['CRU', 'PUY'])[0].short_name == 'CRU'
+    assert get_branch_set('NSHM_v1.0.4', ['CRU', 'PUY'])[1].short_name == 'PUY'
+    assert len(get_branch_set('NSHM_v1.0.4', ['CRU'])) == 1
+
+
+def test_get_branch_set_with_single():
+    assert len(get_branch_set('NSHM_v1.0.4', 'CRU')) == 1
+    assert get_branch_set('NSHM_v1.0.4', 'CRU')[0].short_name == 'CRU'
+
+
+def test_get_branch_set_with_null():
+    assert len(get_branch_set('NSHM_v1.0.4', [])) == 4
+    assert len(get_branch_set('NSHM_v1.0.4')) == 4
 
 
 def test_get_branch_set_invalid_model_version():
     with pytest.raises(ValueError):
-        get_branch_set('', 'CRU')
+        get_branch_set('', ['CRU', 'PUY'])
 
 
 def test_get_branch_set_invalid_branch():
     with pytest.raises(ValueError):
-        get_branch_set('NSHM_v1.0.4', 'AAA')
+        get_branch_set('NSHM_v1.0.4', ['XXX'])
+        get_branch_set('NSHM_v1.0.4', 'XXX')
