4a385ea277f25ba1512eb32116c21ac950bb54e2
ZeyuanC <z.chen@gns.cri.nz>
2024-02-09 17:53:57 +1300

    Merged and fixed mypy error

--- nzshm_model/branch_prop.py
+++ nzshm_model/branch_prop.py
@@ -8,7 +8,9 @@ from nzshm_model.logic_tree import SourceBranch, SourceBranchSet
 from nzshm_model.model_version import get_model_version
 
 
-def get_source_branch_sets(model_version: str, short_names: Union[List[str], str, None] = None) -> Iterator['SourceBranchSet']:
+def get_source_branch_sets(
+    model_version: str, short_names: Union[List[str], str, None] = None
+) -> Iterator['SourceBranchSet']:
     """
     get the SourceBranchSets for the specific model version, and branch set name(s).
 
@@ -26,8 +28,10 @@ def get_source_branch_sets(model_version: str, short_names: Union[List[str], str
     Yields:
         iterator of branch_set objects
     """
-    # if type(short_names) == str:
-    list_short_names: List[str] = [short_names] if type(short_names) == str else short_names
+    if isinstance(short_names, str):
+        list_short_names: List[str] = [short_names]
+    else:
+        list_short_names = short_names if short_names is not None else []
 
     model = get_model_version(model_version)
     if model is None:
@@ -38,7 +42,9 @@ def get_source_branch_sets(model_version: str, short_names: Union[List[str], str
             yield branch_set
     else:
         # user has passes a list of short_names
-        for branch_set in filter(lambda item: item.short_name in list_short_names, model.source_logic_tree().branch_sets):
+        for branch_set in filter(
+            lambda item: item.short_name in list_short_names, model.source_logic_tree().branch_sets
+        ):
             yield branch_set
 
 
@@ -56,33 +62,6 @@ def get_source_branches(model_version: str, short_names: list = None) -> Iterato
     for branch_set in get_source_branch_sets(model_version, short_names):
         for branch in branch_set.branches:
             yield (branch)
-
-
-# def get_branches(model_version: str, branch_set_short_name_list: list = None):
-#     model = get_model_version(model_version)
-#     if model is not None:
-#         slt = model.source_logic_tree().branch_sets
-#         if branch_set_short_name_list is None or len(branch_set_short_name_list) == 0:
-#             branch_set_short_name_list = ['CRU', 'PUY', 'HIK', 'SLAB']
-#         if type(branch_set_short_name_list) == str:
-#             branch_set_short_name_list = [branch_set_short_name_list]
-#         for each_short_name in branch_set_short_name_list:
-#             try:
-#                 output = [next(filter(lambda item: item.short_name == each_short_name, slt))]
-#             except StopIteration:
-#                 raise ValueError("The branch " + each_short_name + " was not found.")
-#             for item in output:
-#                 print(item.short_name + ": ")
-#                 for each_branch in item.branches:
-#                     each_source = each_branch.sources[0]
-#                     if isinstance(each_source, InversionSource):
-#                         print(each_source.inversion_id, each_branch.values, each_branch.weight)
-#                     else:
-#                         print("There are no InversionSource, so VALUES and WEIGHT are: ")
-#                         print(each_branch.values, each_branch.weight)
-#         return "Branches retrieved successfully"
-#     else:
-#         raise ValueError("The model " + model_version + " is not valid.")
 
 
 if __name__ == '__main__':
