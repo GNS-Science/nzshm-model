98a9ab42cf138d332e2d822f20a5e85bb35249c4
Christopher DiCaprio <christopher.dicaprio@gmail.com>
2024-02-18 08:51:37 +1300

    include correlation classes in logic_tree init

--- nzshm_model/logic_tree/__init__.py
+++ nzshm_model/logic_tree/__init__.py
@@ -14,3 +14,4 @@ Classes:
 
 from .gmcm_logic_tree import GMCMBranch, GMCMBranchSet, GMCMLogicTree
 from .source_logic_tree import InversionSource, SourceBranch, SourceBranchSet, SourceLogicTree, SourceLogicTreeV1
+from .logic_tree_base import Correlation, LogicTreeCorrelations
