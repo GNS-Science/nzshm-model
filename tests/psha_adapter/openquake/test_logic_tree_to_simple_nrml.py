c639b98faa994a95510f9776a8e56a770dffa686
chrisdicaprio <christopher.dicaprio@gmail.com>
2024-08-02 17:29:40 +1200

    write_config() writes gmcm file

--- tests/psha_adapter/openquake/test_logic_tree_to_simple_nrml.py
+++ tests/psha_adapter/openquake/test_logic_tree_to_simple_nrml.py
@@ -5,6 +5,7 @@ from pathlib import Path
 
 import pytest
 
+import nzshm_model.psha_adapter.openquake.simple_nrml
 from nzshm_model.logic_tree import GMCMLogicTree, SourceLogicTree
 from nzshm_model.psha_adapter.openquake import OpenquakeSimplePshaAdapter
 
@@ -21,6 +22,26 @@ def test_source_logic_tree_to_source_xml_basic():
     target = FIXTURE_PATH  # noqa
     src_xml = slt.psha_adapter(provider=OpenquakeSimplePshaAdapter).write_config(Path('/tmp/DEMO'))  # noqa
     # assert 0
+
+
+@pytest.mark.skip("WIP")
+def test_fetch_resources(monkeypatch):
+    def mockreturn(file_id, destination):
+        return file_id
+
+    config = FIXTURE_PATH / 'source_logic_tree_sample_2.json'
+    slt = SourceLogicTree.from_json(config)
+
+    monkeypatch.setattr(nzshm_model.psha_adapter.openquake.simple_nrml, "fetch_toshi_source", mockreturn)
+    with tempfile.TemporaryDirectory() as tmpdir:
+        result = [
+            {'id': _id, 'path': filepath, 'um': um}
+            for _id, filepath, um in slt.psha_adapter(provider=OpenquakeSimplePshaAdapter).fetch_resources(tmpdir)
+        ]
+    import json
+
+    with Path('test.json').open('w') as jsonfile:
+        json.dump(result, jsonfile)
 
 
 # @pytest.mark.skip("WIP: need way to compare xml without being exact (or create fixture for exact match)")
