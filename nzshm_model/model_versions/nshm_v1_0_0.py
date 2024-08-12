18a6bac64f43dc16121c5db7bcc3bdda08ac4262
chrisdicaprio <christopher.dicaprio@gmail.com>
2024-08-12 13:39:43 +1200

    official model versions

---
+++ nzshm_model/model_versions/nshm_v1_0_0.py
@@ -0,0 +1,9 @@
+#! nshm_v1_0_0.py
+
+model_args = dict(
+    version='NSHM_v1.0.0',
+    title="Initial version",
+    slt_json="nshm_v1.0.0.json",
+    gmm_json="gmcm_nshm_v1.0.0.json",
+    gmm_xml="NZ_NSHM_GMM_LT_final_EE_new_names.xml",
+)
---
+++ nzshm_model/model_versions/nshm_v1_0_4.py
@@ -0,0 +1,9 @@
+#! nshm_v1_0_4.py
+
+model_args = dict(
+    version='NSHM_v1.0.4',
+    title="NSHM version 1.0.4, corrected fault geometry",
+    slt_json="nshm_v1.0.4_v2.json",
+    gmm_json="gmcm_nshm_v1.0.4.json",
+    gmm_xml="NZ_NSHM_GMM_LT_final_EE_new_names.xml",
+)
