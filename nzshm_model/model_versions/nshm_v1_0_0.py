d47b2201fea3eedef054cea99b71de7480d02644
chrisdicaprio <christopher.dicaprio@gmail.com>
2024-08-15 10:35:37 +1200

    add doc strings

--- nzshm_model/model_versions/nshm_v1_0_0.py
+++ nzshm_model/model_versions/nshm_v1_0_0.py
@@ -1,4 +1,15 @@
 #! nshm_v1_0_0.py
+"""
+This module defines a factory function for generating an `NshmModel` object for the model version NSHM_v1.0.0.
+The dict returned by the function is passed as keyword arguments to the `NshmModel` constructor:
+
+    >>> from nzshm_model import NshmModel
+    >>> from nzshm_model.model_versions.nshm_v1_0_0 import model_args_factory
+    >>> model = NshmModel(**model_args_factory())
+
+NB library users will typically never use this, rather they will obtain a model instance
+using function: `nzshm_model.get_model_version`.
+"""
 from typing import Any, Dict
 
 from nzshm_model.psha_adapter.openquake.hazard_config import OpenquakeConfig
